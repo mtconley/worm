09ac515c8f19df205050dfb33c6f301ecb9c279d
Matthew Conley <mconley@teslamotors.com>
2015-10-23 10:28:45 -0700

    add setup support

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import versioneer
+import sys
+
+config = {
+    'description': 'worm is a library for multiprocessing with pandas DataFrames',
+    'author': 'Matthew Conley',
+    'url': 'https://github.com/mtconley/worm',
+    'download_url': 'https://github.com/mtconley/worm.git',
+    'author_email': '',
+    'version': versionerr.get_version(),
+    'install_requires': [ ],
+    'packages': find_packages(),
+    'name': 'worm'
+}
+
+print "system is: " + sys.platform
+print ''
+print "installing dependencies... "
+print config['install_requires']
+
+setup(**config)
