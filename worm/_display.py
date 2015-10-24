8ba9f87cd656d52468a9daa1275716d4c7304516
Matthew Conley <mconley@teslamotors.com>
2015-10-24 14:39:39 -0700

    only support progressbar in ipython notebook

--- worm/_display.py
+++ worm/_display.py
@@ -11,9 +11,49 @@ class Status(StringIO):
         self.container = {}
         self.counter = {}
         self.total = total
-            
+        self._in_nb = self._is_notebook()
+
+    def _is_notebook(self):
+        try:
+            from IPython.core.interactiveshell import InteractiveShell
+            from IPython.kernel.zmq.zmqshell import ZMQInteractiveShell as notebook
+            from IPython.terminal.interactiveshell import TerminalInteractiveShell as shell
+            if InteractiveShell.initialized():
+                ip = get_ipython()
+                if isinstance(ip, notebook):
+                    return True
+                elif isinstance(ip, shell):
+                    return False
+                else:
+                    raise Exception('Wrong Shell')
+            else:
+                return False
+        except Exception as e:
+            self.print_error(e)
+            return False
+
+    def remaining_time(self):
+        pass
+
+    def elapsed_time(self):
+        pass
+
     def fmt(self, prog):
-        string = '<progress value="{prog:2.2f}" max="100"></progress>'.format(prog=prog)
+        if self._in_nb:
+            return self._nb_fmt(prog)
+        else:
+            return self._console_fmt(prog)
+            
+    def _nb_fmt(self, prog):
+        string = '<progress value="{prog:2.2f}" max="100"></progress>'
+        string = string.format(prog=prog)
+        return string
+
+    def _console_fmt(self, prog):
+        full = (prog) / 3
+        empty = 33 - full
+        progress = '#' * full + ' ' * empty
+        string = '[' + progress + ']'
         return string
 
 
@@ -21,16 +61,20 @@ class Status(StringIO):
         try:
             self.counter[name] = self.counter.get(name, len(self.counter))
             lineno = self.counter[name]
+
             self.container[lineno] = self.container.get(lineno, {'name': name, 'count': 0})
             self.container[lineno]['count'] += 1
+
             count = self.container[lineno]['count']
             interval = int(.05 * self.total)
             interval = interval if interval > 0 else 1
+
             if (count %  interval == 0):
                 clear_output(True)
                 self.flush()
         except Exception as e:
             self.print_error(e)
+
 
     def flush(self):
         try:
@@ -39,10 +83,14 @@ class Status(StringIO):
             df = DataFrame(data).T
             df['progress'] = df['count'].astype(int) / self.total * 100
             df['update'] = df['progress']
-            df['update'] = df['update'].apply(self.fmt)
             df.sort_index(inplace=True)
             fields = ['name', 'progress', 'update']
-            display(HTML(df[fields].to_html(escape=False)))
+            df['update'] = df['update'].apply(self.fmt)
+            board = df[fields]
+
+            if self._in_nb:
+                display(HTML(board.to_html(escape=False)))
+
         except Exception as e:
             self.print_error(e)
 
