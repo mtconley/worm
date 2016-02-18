b3d673f8a81cc55eabeee3e55287b21c2233358c
Matthew Conley <matthewt.conley@gmail.com>
2016-02-17 16:12:58 -0800

    touch

---
+++ worm/_record.py
@@ -0,0 +1,35 @@
+from multiprocessing import current_process
+
+class Record(object):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
+    def update(self, attrs):
+        self.__dict__.update(attrs)
+        return self
+
+    def __getitem__(self, item):
+        if item in self.__dict__:
+            return self.__dict__[item]
+        else:
+            return None
+
+    def __setitem__(self, name, value):
+        self.__dict__[name] = value
+        
+
+    def __repr__(self):
+        return 'Record({})'.format(repr(self.__dict__))
+
+class RecordHandler(object):
+    def __init__(self, funcs):
+        self.funcs = funcs
+
+    def __call__(self, data):
+        for func in self.funcs:
+            data = func(data)
+        name = current_process().name
+        msg = {'name': name,
+               'data': data}
+        return msg
